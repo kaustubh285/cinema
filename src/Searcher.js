19f826c18083dbe69dd97c8cff95e6f6e3ce5662
kaustubh285 <kaustubh285@gmail.com>
2020-06-20 11:27:32 +0530

    Rendering movie card using movie component

--- src/Searcher.js
+++ src/Searcher.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Movies from "./Movies";
 
 export default function Searcher() {
   const [query, setQuery] = useState("");
@@ -41,42 +42,7 @@ export default function Searcher() {
 
       <div className="">
         {movies.map((movie) => (
-          <div
-            className="bg-white container border mb-2 mt-1 shadow-sm rounded"
-            // style={{ backgroundColor: "rgba(255, 242, 230,0.1)" }}
-            key={movie.id}
-          >
-            <div className="row ">
-              <div className="col-lg-12 d-flex justify-content-between">
-                <p>
-                  <small className="Movie-titles">
-                    <b>
-                      <u>{movie.title}</u>
-                    </b>
-                  </small>
-                  <span></span>
-                  <small> {"  (" + movie.release_date + ")"}</small>
-                </p>
-                <p>
-                  <small>{"Movie rating: " + movie.vote_average}</small>
-                </p>
-              </div>
-            </div>
-            <div className="row flex-column-reverse flex-md-row">
-              <div className="col-lg-9">
-                <p>
-                  <small>{movie.overview}</small>
-                </p>
-              </div>
-              <div className="col-lg-3">
-                <img
-                  className=""
-                  src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-                  alt={movie.title + " poster"}
-                />
-              </div>
-            </div>
-          </div>
+          <Movies movie={movie} key={movie.id} />
         ))}
       </div>
     </div>
