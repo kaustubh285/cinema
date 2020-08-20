d62dc3ee8b794857522668d75dd9a8c1ca70005a
kaustubh <kaustubh285@gmail.com>
2020-08-20 12:51:32 +0530

    Installed material-ui, changed button to material ui button, added error div for no query

--- src/Searcher.js
+++ src/Searcher.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Movies from "./Movies";
+import { Button } from "@material-ui/core";
 
 export default function Searcher() {
   const [query, setQuery] = useState("");
@@ -7,44 +8,59 @@ export default function Searcher() {
   const [movies, setMovies] = useState([]);
   const searchMovies = async (event) => {
     event.preventDefault();
-    console.log("Submitting the query!!");
+    var errorDiv = document.getElementById("errordiv");
+    if (query === "") {
+      errorDiv.setAttribute("class", "d-block ");
+    } else {
+      errorDiv.setAttribute("class", "d-none alert");
+      console.log("Submitting the query!!");
 
-    console.log("submitting");
+      console.log("submitting");
 
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=0d3ca7edae2d9cb14c86ce991530aee6&language=en-US&query=${query}&page=1&include_adult=false`;
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      console.log(data.results);
-      setMovies(data.results);
-    } catch (err) {
-      console.error(err);
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=0d3ca7edae2d9cb14c86ce991530aee6&language=en-US&query=${query}&page=1&include_adult=false`;
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        console.log(data.results);
+        setMovies(data.results);
+      } catch (err) {
+        console.error(err);
+      }
     }
   };
 
   return (
-    <div>
-      <form className="Form pb-2" onSubmit={searchMovies}>
-        <label htmlFor="query" className="label">
-          Movie Name
-        </label>
-        <input
-          type="text"
-          name="query"
-          placeholder="i.e star wars"
-          value={query}
-          onChange={(event) => setQuery(event.target.value)}
-        ></input>
-        <button type="submit" className="btn btn-dark mybutton">
-          Search
-        </button>
-      </form>
-
-      <div className="">
-        {movies.map((movie) => (
-          <Movies movie={movie} key={movie.id} />
-        ))}
+    <>
+      <div className='d-none' id='errordiv'>
+        <p className='text-center alert alert-danger'>
+          Please enter Movie Name
+        </p>
       </div>
-    </div>
+      <div>
+        <form className='Form pb-2' onSubmit={searchMovies}>
+          <label htmlFor='query' className='label'>
+            Movie Name
+          </label>
+          <input
+            type='text'
+            name='query'
+            placeholder='i.e star wars'
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}></input>
+          <Button type='submit' color='default' size='large' variant='outlined'>
+            Search
+          </Button>
+          {/* <button type="submit" className="btn btn-dark mybutton">
+          Search
+        </button> */}
+        </form>
+
+        <div className=''>
+          {movies.map((movie) => (
+            <Movies movie={movie} key={movie.id} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
